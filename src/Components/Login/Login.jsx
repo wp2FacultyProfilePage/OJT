b69d7b5044812cf75cfd660ddaa6ac340cad48eb
wp2FacultyProfilePage <145127473+wp2FacultyProfilePage@users.noreply.github.com>
2024-01-31 19:10:08 +0800

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,36 +1,39 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-//import './Login.css'; // Create a separate CSS file and import it
+import './Login.css'; // Create a separate CSS file and import it
 
 function LoginForm() {
   return (
     <div className="login-container">
-      <h2>Login</h2>
+      <h2>LOGIN</h2>
+
       <form className="login-form">
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
+        
+        <div className="mb-3">
+          <input type="text" id="username" name="username" placeholder='Username...' required />
         </div>
-
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+        <div className="mb-3">
+          <input type="password" id="password" name="password" placeholder='Password...'  required />
         </div>
-
+        <div>
+          <input type="checkbox" id="rememberMe" name="rememberMe" />
+          <label id="rememberMe">Remember me</label>
+        </div>
+        <div className="mb-3">
+          <button type="submit" id="submitbtn">LOGIN</button>
+        </div>
         <div className="form-options">
-          <div>
-            <input type="checkbox" id="rememberMe" name="rememberMe" />
-            <label htmlFor="rememberMe">Remember me</label>
+          <div id="forgot" >
+            <a href="#">Forgot password?</a>
           </div>
-          <a href="#">Forgot password?</a>
+          <div id="register">
+            <p>Don't have an account? <a href="#">Register</a></p>
+          </div>
         </div>
-
-        <button type="submit">Login</button>
+        
       </form>
 
-      <div className="form-options">
-        <p>Don't have an account? <a href="#">Register</a></p>
-      </div>
+      
     </div>
   );
 }
