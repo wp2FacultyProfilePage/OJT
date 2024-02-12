af1e4646233d1f3e2e416da22aacea63da64fd14
wp2FacultyProfilePage <145127473+wp2FacultyProfilePage@users.noreply.github.com>
2024-02-12 00:50:37 -0700

    Add files via upload

---
+++ src/components/first.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div>
+     
+        <ul class="nav-routes">
+                    <RouterLink to="/">Home </RouterLink>
+                    <!-- <RouterLink to="/about"> About </RouterLink> -->
+
+        </ul>
+    </div>
+</template>
+
+
+
+
+<style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  padding: 10px 16px;
+}
+
+.branding {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.branding img {
+  max-width: 50px;
+}
+
+.branding h1 {
+  font-size: 20px;
+}
+
+.nav-routes {
+  display: flex;
+  justify-content: flex-end;
+  gap: 12px;
+  list-style: none;
+}
+
+.nav-routes a {
+  text-decoration: none;
+  color: black;
+}
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>This is Login</h1>
+    <div class="auth-container">
+    <h2>Login</h2>
+    <form @submit.prevent="login" class="auth-form">
+      <label for="username">Username:</label>
+      <input type="text" id="username" v-model="username" required />
+      
+      <label for="password">Password:</label>
+      <input type="password" id="password" v-model="password" required />
+      <RouterLink to="/about"> <button type="submit">Login</button> </RouterLink>
+      
+    </form>
+  </div>
+  </main>
+</template>
+
+<style scoped>
+.auth-container {
+  max-width: 400px;
+  margin: auto;
+  padding: 20px;
+  background-color: #f4f4f4;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.auth-form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.auth-form label {
+  font-weight: bold;
+}
+
+.auth-form input {
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.auth-form button {
+  padding: 10px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.auth-form button:hover {
+  background-color: #45a049;
+}
+</style>
